dd59320acf7fa28b9f86a8fb38dbd5468a5a3387
Bieky Mahato <biekymahato@gmail.com>
2020-06-01 15:38:01 +0530

    Adding Comments to the DB

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ router.get('/', homeController.home);
 router.use('/users', require('./users'));
 //for post router
 router.use('/posts', require('./posts'));
-
+router.use('/comments', require('./comments'));
 //for any further routes, access from here
 //router.use('/routerName', require())
 
