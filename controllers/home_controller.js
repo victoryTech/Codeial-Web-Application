3a88314307027f30074b10d2f5dadb645d7a52e8
Bieky Mahato <biekymahato@gmail.com>
2020-06-01 09:54:24 +0530

    Dislay Posts and related User

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,15 +1,32 @@
+const Post = require('../models/post');
+
 module.exports.home = function(req, res){
 
     //to check cookies which added from chrome and to print in console
-    console.log(req.cookies);
+    //console.log(req.cookies);
 
     //to change the cookies and send
-    res.cookie('something', 30);
+    //res.cookie('something', 30);
 
     //return res.end('<h1>Express is up for Codeial</h1>');
-    //to check view engine is running
-    return res.render('home', {
-        title: "Home"
+    
+    // to show post in home without user information
+    //   Post.find({}, function(err, posts){
+    //     return res.render('home', {
+    //         title: "Codial | Home",
+    //         posts: posts
+    //   });
+    
+    // });
+
+    //populate the user of each post
+    //to get info of user which having post in home
+    Post.find({}).populate('user').exec(function(err, posts){
+        
+        return res.render('home', {
+            title: "Codial | Home",
+            posts: posts
+      });
     });
 }
 
