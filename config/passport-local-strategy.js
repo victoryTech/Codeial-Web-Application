cf0258a0c9545c075ea601bfca51396edd5fb2f8
Bieky Mahato <biekymahato@gmail.com>
2020-06-05 19:39:42 +0530

    Adding flash messages to more action such as in posting and deleting a post and many more...

--- config/passport-local-strategy.js
+++ config/passport-local-strategy.js
@@ -5,54 +5,58 @@ const LocalStrategy = require('passport-local').Strategy;
 const User = require('../models/user');
 
 
-//authentication using passport
+// authentication using passport
 passport.use(new LocalStrategy({
-    usernameField: 'email'
-},
-function(email, password, done){
-    // find a user and establish the identify
-    User.findOne({email: email}, function(err, user){
-        if(err){
-            console.log('Error in finding user --> Passport');
-            return done(err);
-        }
+        usernameField: 'email',
+        passReqToCallback: true
+    },
+    function(req, email, password, done) {
+        // find a user and establish the identity
+        User.findOne({ email: email }, function(err, user) {
+            if (err) {
+                // cosole.log('Error in findingh user --> Passport');
+                req.flash('error', err);
+                return done(err);
+            }
 
-        if(!user || user.password != password){
-            console.log('Invalid Username/Password');
-            return done(null, false);
-        }
+            if (!user || user.password != password) {
+                //console.log('Invalid Username/passport');
+                req.flash('error', 'Invalid Username/Password');
+                return done(null, false);
+            }
 
-        return done(null, user);
+            return done(null, user);
+        });
+    }
 
 
-    });
-}
 ));
 
-//serializing the user to decide which key is to be kept in the cookies
-passport.serializeUser(function(user, done){
+
+// serializing the user to decide which key is to be kept in the cookies
+passport.serializeUser(function(user, done) {
     done(null, user.id);
 });
 
 
-//deserializing the user from the key in the cookies
-passport.deserializeUser(function(id, done){
-    User.findById(id, function(err, user){
-        if(err){
-            console.log('Error in finding user --> passport');
+
+// deserializing the user from the key in the cookies
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        if (err) {
+            console.log('Error in finding user --> Passport');
             return done(err);
         }
 
         return done(null, user);
     });
-    
 });
 
 
-//check if the user in authentication
-passport.checkAuthentication = function(req, res, next){
+// check if the user is authenticated
+passport.checkAuthentication = function(req, res, next) {
     // if the user is signed in, then pass on the request to the next function(controller's action)
-    if(req.isAuthenticated()){
+    if (req.isAuthenticated()) {
         return next();
     }
 
@@ -60,15 +64,15 @@ passport.checkAuthentication = function(req, res, next){
     return res.redirect('/users/sign-in');
 }
 
-
-passport.setAuthenticatedUser = function(req, res, next){
-    if(req.isAuthenticated()){
+passport.setAuthenticatedUser = function(req, res, next) {
+    if (req.isAuthenticated()) {
         // req.user contains the current signed in user from the session cookie and we are just sending this to the locals for the views
         res.locals.user = req.user;
     }
 
     next();
 }
+
 
 
 module.exports = passport;
