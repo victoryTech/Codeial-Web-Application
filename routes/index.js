c5537255d15416999c5ea3da886ee272edf3e9a5
Bieky Mahato <biekymahato@gmail.com>
2020-06-10 01:07:48 +0530

    Setting up the directory structure for api(postman)

--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,11 @@ router.use('/users', require('./users'));
 //for post router
 router.use('/posts', require('./posts'));
 router.use('/comments', require('./comments'));
+
 //for any further routes, access from here
 //router.use('/routerName', require())
 
+// for api router
+router.use('/api', require('./api'));
 
 module.exports = router;
